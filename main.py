5ab7ad6c0ecdaa87f8550a114e829b716ea2e33b
Joseph Jackson <admin@kamiak.org>
2022-02-13 00:00:54 -0800

    uwu

--- main.py
+++ main.py
@@ -342,8 +342,8 @@ async def on_message(message):
           for student in data
           for matches in [sum(
             next((
-              5 if len(param) == len(name) else 1
-              for name in student.name.lower().split(' ')
+              (5 if len(param) == len(name) else 1)*3125/(5*(name_i+1))
+              for name_i, name in enumerate(student.name.lower().split(' '))
               if param in name
             ), 0)
             for param in query
