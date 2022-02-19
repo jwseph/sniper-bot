eee3f30dec145b49708832465da26ba7d51d056b
unknown <admin@kamiak.org>
2022-02-18 17:31:11 -0800

    uwu

--- index.py
+++ index.py
@@ -2,14 +2,6 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 
-async def app(scope, receive, send):
-    """The simplest of ASGI apps, displaying scope."""
-    headers = [(b"content-type", b"text/html")]
-    body = open('index.html', 'rb').read()
-    await send({"type": "http.response.start", "status": 200, "headers": headers})
-    await send({"type": "http.response.body", "body": body})
-
-
 app = FastAPI()
 app.mount("/", StaticFiles(directory="."), name="static")
 
