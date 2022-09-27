e0dd7d7bbbcdbe462a7ca3f00460c8e02773e9a0
kamiakhs <admin@kamiak.org>
2022-09-26 17:20:52 -0700

    a

--- main.py
+++ main.py
@@ -214,7 +214,6 @@ class MudaeView(discord.ui.View):
 
   async def on_claim(self, interaction:discord.Interaction):
     self.button.disabled = True
-    await self.update(interaction)
     self.embed.set_footer('Why are you doing this', icon_url=interaction.user.display_avatar.url)
     self.embed.color = 0x670d08
     await interaction.response.edit_message(embed=self.embed, view=self)
